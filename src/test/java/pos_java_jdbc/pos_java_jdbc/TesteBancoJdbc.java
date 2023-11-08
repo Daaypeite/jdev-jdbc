6270b14b1aeb125decfd15096a25c589cb9afdbd
Daaypeite <daiane.peite@hotmail.com>
2023-11-08 17:53:50 -0300

    Criando nossa consulta SQL ao banco de dados

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,5 +1,7 @@
 package pos_java_jdbc.pos_java_jdbc;
 
+import java.util.List;
+
 import org.junit.Test;
 import dao.UserPosDAO;
 import model.Userposjava;
@@ -17,5 +19,35 @@ public class TesteBancoJdbc{
 		
 		userPosDAO.salvar(userposjava);
 		
+	}
+	
+	@Test
+	public void initListar() {
+		UserPosDAO dao = new UserPosDAO();
+		try {
+			List<Userposjava> list = dao.listar();
+			
+		 for (Userposjava userposjava : list) {
+			 System.out.println(userposjava.getNome());
+			 System.out.println("---------------------------------------");
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void initBuscar() {
+		
+		UserPosDAO dao = new UserPosDAO();
+		
+		try {
+			Userposjava userposjava = dao.buscar(5L);
+			
+			System.out.println(userposjava); 
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
