35a7c6acdbf94dba9883916c90b42c95e15d9097
Daaypeite <daiane.peite@hotmail.com>
2023-11-09 17:25:50 -0300

    Deletando registro no banco de dados

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -6,64 +6,76 @@ import org.junit.Test;
 import dao.UserPosDAO;
 import model.Userposjava;
 
-public class TesteBancoJdbc{
+public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
+
 		UserPosDAO userPosDAO = new UserPosDAO();
 		Userposjava userposjava = new Userposjava();
-		
-		userposjava.setId(6L);
+
 		userposjava.setNome("Paulo teste");
 		userposjava.setEmail("paulo@gamil.com");
-		
+
 		userPosDAO.salvar(userposjava);
-		
+
 	}
-	
+
 	@Test
 	public void initListar() {
 		UserPosDAO dao = new UserPosDAO();
 		try {
 			List<Userposjava> list = dao.listar();
-			
-		 for (Userposjava userposjava : list) {
-			 System.out.println(userposjava.getNome());
-			 System.out.println("---------------------------------------");
-		}
+
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava.getNome());
+				System.out.println("---------------------------------------");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void initBuscar() {
-		
+
 		UserPosDAO dao = new UserPosDAO();
-		
+
 		try {
 			Userposjava userposjava = dao.buscar(5L);
-			
-			System.out.println(userposjava); 
-			
+
+			System.out.println(userposjava);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void initAtualizar() {
 		try {
-		UserPosDAO dao = new UserPosDAO();
-		
-		Userposjava objetoBanco = dao.buscar(5L);
-		
-		objetoBanco.setNome("Nome mudado com o metodo atualizar");
-		
-		dao.atualizar(objetoBanco);
-		
-		}catch (Exception e) {
+			UserPosDAO dao = new UserPosDAO();
+
+			Userposjava objetoBanco = dao.buscar(5L);
+
+			objetoBanco.setNome("Nome mudado com o metodo atualizar");
+
+			dao.atualizar(objetoBanco);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void initDeletar() {
+		try {
+
+			UserPosDAO dao = new UserPosDAO();
+			dao.deletar(6L);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
